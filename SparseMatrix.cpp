35b8881284deafceb18daddc86591499fc8a485e
knightflight377 <emholm12@gmail.com>
2016-05-10 15:44:50 -0500

    Changed print function, started working on SolveSystem and its helper functions, changed testing code and noticed that the copy constructor still doesn't work.

--- SparseMatrix.cpp
+++ SparseMatrix.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include "SparseRow.C"
+#include <cmath>
 using namespace std;
 
 //This is the SparseMatrix class
 //The matrix is represented as a vector that contains pointers to rows from the SparseRow class
-
-//typedef std::list <std::pair <int, double > > *rowPtr;
+//This class also assumes that the matrix is a square matrix (# of rows = # of columns)
 
 class SparseMatrix {
 	public:
@@ -20,7 +21,7 @@ class SparseMatrix {
 	//Variables
 	int rows; //Number of rows in matrix A
 	int ncols; //Number of columns in matrix A
-	//vector <double> b; //The vector of structural constraint values, pass this as an argument to solveSystem
+	vector <double> b; //The vector of structural constraint values, pass this as an argument to solveSystem
 	double t; //This is the zero tolerance
 	vector < Row* > vectOfRowPointers; //The vector of Row pointers
 
@@ -30,9 +31,12 @@ class SparseMatrix {
 	bool interchangeRow(int, int); //Switch the given rows
 	bool multiplyByScaler(int, double); //Multiply the given row by a scaler
 	bool combineRows(int, int, double); //Replace row with itself + constant*second row
-	//vector SolveSystem(vector); //Solves the linear system
+	//vector SolveSystem(vector<double> &b); //Solves the linear system
 	void printMatrix(); //Prints the matrix
 
+	private:
+	//bool pivot(int , vector<double> &b); //Function that performs the pivot operation
+	//bool pivotGetZeros(int, vector<double> &b); //Function to get 0s in every row except the pivot row
 };
 
 //Constructor
@@ -54,7 +58,7 @@ SparseMatrix::SparseMatrix (int r, double tolerance) {
 	}
 };
 
-//Copy constructor
+//Copy constructor, DEBUG THIS!
 SparseMatrix:: SparseMatrix(const SparseMatrix& a) {
 	rows = a.rows;
 	ncols = a.ncols;
@@ -68,7 +72,6 @@ SparseMatrix:: SparseMatrix(const SparseMatrix& a) {
 		Row tempRow = (*vectOfRowPointers[m]);
 		vectOfRowPointers[m] = &tempRow;
 	}
-
 };
 
 //Destructor -- free up the used memory
@@ -143,64 +146,184 @@ bool SparseMatrix:: interchangeRow(int r1, int r2) {
 
 //Multiply r1 with r1 + constant*r2
 //r1 and r2 are row indices
-// bool SparseMatrix:: combineRows(int r1, int r2 , double constant) {
-// 	if (r1 >= 0 && r1 < rows && r2 >= 0 && r2 < rows) {
-// 		(*vectOfRowPointers[r1]).replaceRow((*vectOfRowPointers[r2]), constant);
-// 		return true;
-// 	}
-// 	else {
-// 		return false;
-// 	}
-// };
+bool SparseMatrix:: combineRows(int r1, int r2 , double constant) {
+	//check that r1 and r2 are valid row indices
+	if (r1 >= 0 && r1 < rows && r2 >= 0 && r2 < rows) {
+		//get the rows and call replaceRow from SparseRow.C
+		(*vectOfRowPointers[r1]).replaceRow((*vectOfRowPointers[r2]), constant);
+		return true;
+	}
+	else {
+		return false;
+	}
+};
 
 //Solve the linear system
+//Uses the pivot function and pivotGetZeros function
 //If no solution exists, return the empty vector
-//vector SparseMatrix:: SolveSystem(vector b) {
-
-//};
+// vector SparseMatrix:: SolveSystem(vector<double> &b) {
+// 	bool returnValue;
+// 	for (int i = 0; i < rows; i++) {
+// 		bool returnValue = pivot(i, &b);
+// 		if (!returnValue) {
+// 			cout << "There is no solution" << endl;
+// 		}
+// 		else {
+// 			cout << "The solution is: " b << endl;
+// 		}
+// 	}
+// };
 
 //Print the matrix
 void SparseMatrix:: printMatrix() {
 	for (int j = 0; j < rows; j++) {
-		//if a row is empty, print out a row of zeros
-		if ((*vectOfRowPointers[j]).lengthOfRow() == 0) {
-			for (int n = 0; n < rows; n++) {
+		//Get each row and set up an iterator
+		SpRow rprint = (*vectOfRowPointers[j]).getRow();
+		SpRow:: iterator it = rprint.begin();
+		for (int m = 0; m < rows; m++) {
+			//If the column index exists in the row, then the associated value is nonzero
+			if ((*it).first == m) {
+				cout << (*it).second << " ";
+			}
+			else {
 				cout << "0" << " ";
 			}
 		}
-		else {
-		//figure out how to print out rows with nonzero entries
-		}
+		cout << endl;
 	}
+	cout << endl;
 };
+
+//Use retrieveValue in this function
+//This function finds the row with the greatest value at the given column index and pivots
+//c is the column index
+// bool SparseMatrix:: pivot(int c, vector<double> &b) {
+// 	double maxEntry = 0.0;
+// 	int maxEntryRow = 0;
+
+// 	for (int i = 0; i < rows; i++) {
+// 		Row currentRow = (*vectOfRowPointers[i]);
+// 		double temp = currentRow.retrieveValue(c);
+// 		if (abs(temp) > maxEntry) {
+// 			maxEntry = temp;
+// 			maxEntryRow = i;
+// 			interchangeRow(c, maxEntryRow);
+// 			double scaler = 1.0/maxEntry;
+// 			currentRow.multiplyRowByScaler(scaler);
+// 			iter_swap(b.begin() + c, b.begin() + maxEntryRow);
+// 			b[maxEntryRow] = b[maxEntryRow]*scaler;
+// 		}
+// 		else {
+// 			return false;
+// 		}
+// 	}
+// 	pivotGetZeros(c, vector<double> &b);
+// 	return true;
+// };
+
+//Old version of function, DELETE after confirming that new version works
+// bool SparseMatrix:: pivot(int c, vector <double> b){
+// 	double maxEntry = 0.0; //change this later to be the pivot point entry
+// 	int maxEntryRow = 0; //change this later to be the pivot point row
+// 	for (int i = 0; i < rows; i++) {
+// 		Row currentRow = (*vectOfRowPointers[i]);
+// 		SpRow::iterator it = currentRow.begin();
+// 		//If the column index equals c and the absolute value of the value associated with index c
+// 		//is greater than 0, swap the two rows in both the matrix and the vector b
+// 		if ((*it).first == c && abs((*it).second) > maxEntry) {
+// 			//set the max to the new absolute value
+// 			max = abs((*it).second);
+// 			//swap rows c and i to get the maximum value at the top
+// 			interchangeRow(c, i);
+// 			//set the scaler to 1/the max value
+// 			double scaler = 1.0/(*it).second;
+// 			//multiply the row by that scaler so that there's a 1 in position (c,c) of the matrix
+// 			currentRow.multiplyRowByScaler(scaler);
+// 			//do the same swapping and multiplication to the b vector
+// 			iter_swap(b.begin() + c, b.begin() + i);
+// 			b[i] = b[i]*scaler;
+// 		}
+// 		//If it's not possible to find an absolute value greater than 0, return false
+// 		else {
+// 			return false;
+// 		}
+// 	//return true;
+// 	}
+// 	//If the entire for loop is completed, call the pivotGetZeros function and return true;
+// 	pivotGetZeros(c, vector <double> b);
+// 	return true;
+// };
+
+//This function ensures that after the pivot is complete, there is a zero at column index c
+// in every row except the pivot row
+// bool SparseMatrix:: pivotGetZeros(int c, vector <double> &b) {
+// 	//Get the row that's being pivoted on, it will needed later
+// 	Row pivotRow = (*vectOfRowPointers[c]);
+// 	//Every other entry in the column must be a zero
+// 	for (int i = 0; i < rows; i++) {
+// 		//Get the current row
+// 		Row currentRow = (*vectOfRowPointers[i]);
+// 		if (i != c) {
+// 			//We want to leave the pivot row alone, which is why (i != c)
+// 			//We want to get the value at i and use it
+// 			double constantScaler = currentRow.retrieveValue(i);
+// 			//Do currentRow = currentRow + (-constantScaler)*pivotRow
+// 			currentRow.replaceRow(pivotRow, -constantScaler);
+// 			//Do the same operation to the b vector
+// 			b[i] = b[i]*(-constantScaler);
+// 		}
+// 	}
+// 	//If the entire for loop runs, return true
+// 		return true;
+// 	return false;
+// };
 
 int main() {
 
 	cout << "I got here" << endl;
 
-	SparseMatrix a(2, 0.1);
+	//Test of constructor
+	SparseMatrix a(3, 0.1);
 	cout << "Matrix a" << endl;
 	a.printMatrix();
-
 	cout << endl;
+
+	//Test of insertMatrixElement
 	cout << "Inserting into a" << endl;
-	a.insertMatrixElement(1, 1, 2);
+	a.insertMatrixElement(2, 1, 2);
+	a.insertMatrixElement(0, 1, 5);
+	a.insertMatrixElement(1, 0, 4);
 	a.printMatrix();
-
 	cout << endl;
+
+	//Test of multiplyByScaler
 	cout << "Multplying by a scaler" << endl;
 	a.multiplyByScaler(1, 3);
 	a.printMatrix();
-
 	cout << endl;
+
+	//Test of interchangeRow
 	cout << "Swapping rows" << endl;
 	a.interchangeRow(1, 0);
 	a.printMatrix();
-
 	cout << endl;
-	cout << "Deleting from a" << endl;
-	a.deleteMatrixElement(0, 1);
+
+	//Test of combineRows
+	cout << "Replacing Rows" << endl;
+	a.combineRows(1, 2, 3);
 	a.printMatrix();
+
+	//Test of deleteMatrixElement
+	cout << "Deleting from a" << endl;
+	a.deleteMatrixElement(1, 1);
+	a.printMatrix();
+	cout << endl;
+
+	//Test of copy constructor
+	cout << "Using copy constructor" << endl;
+	SparseMatrix acpy(a);
+	acpy.printMatrix();
+	cout << endl;
 
 
 };
