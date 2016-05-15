a2e13359c23ea0bd50ce19224d48cbbdae432de9
knightflight377 <emholm12@gmail.com>
2016-05-15 16:39:49 -0500

    Started debugging pivotGetZeros

--- SparseMatrix.cpp
+++ SparseMatrix.cpp
@@ -266,24 +266,25 @@ bool SparseMatrix:: pivot(int c, vector<double> &b) {
 // in every row except the pivot row
 bool SparseMatrix:: pivotGetZeros(int c, vector <double> &b) {
 	//Get the row that's being pivoted on, it will needed later
-	Row& pivotRow = (*vectOfRowPointers[c]);
-	//cout << "pivotRow" << endl;
-	//pivotRow.print();
+	Row* pivotRow = vectOfRowPointers[c];	
+	//Row& pivotRow = (*vectOfRowPointers[c]);
+	cout << "pivotRow" << endl;
+	pivotRow->print();
 	//Every other entry in the column must be a zero
-	for (int i = 0; i < rows; i++) {
+	for (int i = c; i < rows; i++) {
 		//Get the current row
 		Row& currentRow = (*vectOfRowPointers[i]);
-		//cout << "currentRow in pivotGetZeros function" << endl;
-		//currentRow.print();
-		if (i != c) {
+		cout << "currentRow in pivotGetZeros function" << endl;
+		currentRow.print();
+		if(i != c) {
 			//We want to leave the pivot row alone, which is why (i != c)
 			//We want to get the value at i and use it
 			double constantScalar = currentRow.retrieveValue(i);
-			//cout << "constantScalar from pivotGetZeros function"  << " " << constantScalar << endl;
+			cout << "constantScalar from pivotGetZeros function"  << " " << constantScalar << endl;
 			//Do currentRow = currentRow + (-constantScalar)*pivotRow
-			currentRow.replaceRow(pivotRow, -constantScalar);
-			//cout << "currentRow again in pivotGetZeros function" << endl;
-			//currentRow.print();
+			currentRow.replaceRow(*pivotRow, -constantScalar);
+			cout << "currentRow again in pivotGetZeros function" << endl;
+			currentRow.print();
 			cout << "Matrix t in pivotGetZeros function" << endl;
 			printMatrix();
 			//Do the same operation to the b vector
@@ -294,85 +295,4 @@ bool SparseMatrix:: pivotGetZeros(int c, vector <double> &b) {
 	//If the entire for loop runs, return true
 		return true;
 	return false;
-};
-
-int main() {
-
-	cout << "I got here" << endl;
-
-	//Test of constructor
-	SparseMatrix a(3, 0.1);
-	cout << "Matrix a" << endl;
-	a.printMatrix();
-	cout << endl;
-
-	//Test of insertMatrixElement
-	cout << "Inserting into a" << endl;
-	a.insertMatrixElement(2, 1, 2);
-	a.insertMatrixElement(0, 1, 5);
-	a.insertMatrixElement(1, 0, 4);
-	a.printMatrix();
-	cout << endl;
-
-	//Test of multiplyByScaler
-	cout << "Multplying by a scalar" << endl;
-	a.multiplyByScalar(1, 3);
-	a.printMatrix();
-	cout << endl;
-
-	//Test of interchangeRow
-	cout << "Swapping rows" << endl;
-	a.interchangeRow(1, 0);
-	a.printMatrix();
-	cout << endl;
-
-	//Test of combineRows
-	cout << "Replacing Rows" << endl;
-	a.combineRows(1, 2, 3);
-	a.printMatrix();
-
-	//Test of deleteMatrixElement
-	cout << "Deleting from a" << endl;
-	a.deleteMatrixElement(1, 1);
-	a.printMatrix();
-	cout << endl;
-
-	//Test of copy constructor
-	cout << "Using copy constructor" << endl;
-	SparseMatrix acpy(a);
-	acpy.printMatrix();
-	cout << endl;
-
-	//Test of SolveSystem
-	cout << "Test of SolveSystem" << endl;
-	SparseMatrix t(3, 0.01);
-	//Set up row 0
-	t.insertMatrixElement(0, 0, 2);
-	t.insertMatrixElement(0, 1, 1);
-	t.insertMatrixElement(0, 2, 3);
-	//Set up row 1
-	t.insertMatrixElement(1, 0, 2);
-	t.insertMatrixElement(1, 1, 6);
-	t.insertMatrixElement(1, 2, 8);
-	//Set up row 2
-	t.insertMatrixElement(2, 0, 6);
-	t.insertMatrixElement(2, 1, 8);
-	t.insertMatrixElement(2, 2, 18);
-	cout << "Matrix t" << endl;
-	t.printMatrix();
-	cout << endl;
-
-	//Set up the right-hand side vector
-	vector <double> tt;
-	tt.push_back(1);
-	tt.push_back(3);
-	tt.push_back(5);
-	cout << "Right-hand side vector:" << endl;
-	for (int i = 0; i < 3; i++) {
-		cout << tt[i] << endl;
-	}
-
-	//Call SolveSystem; the answer should be [3/10, 2/5, 0]
-	t.solveSystem(tt);
-
 };
